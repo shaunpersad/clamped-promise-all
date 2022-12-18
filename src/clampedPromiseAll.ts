e7300f96b454b0291cdbed1a40c705aa976242be
Shaun Persad <shaunpersad@gmail.com>
2022-12-18 16:16:11 -0500

    Simplified logic

--- src/clampedPromiseAll.ts
+++ src/clampedPromiseAll.ts
@@ -9,73 +9,55 @@ export type SettledReturnType<Value, Reason> = {
 /**
  * Clamped alternative to Promise.all.
  */
-export function clampedAll<Value>(
+export async function clampedAll<Value>(
   arr: Array<() => Promise<Value> | Value>,
   clamp: number,
 ): Promise<Value[]> {
   const { length } = arr;
   const maxExecutions = Math.min(length, clamp);
-  return maxExecutions === 0 ? Promise.resolve([]) : new Promise((resolve, reject) => {
-    const results: Value[] = Array.from({ length });
-    let numExecutions = 0;
-    let numResults = 0;
-    let isFinished = false;
-    const execute = () => {
-      const index = numExecutions++;
-      return Promise.resolve()
-        .then(() => arr[index]())
-        .then((result) => {
-          numResults++;
-          if (!isFinished) {
-            results[index] = result;
-            if (numExecutions < length) {
-              void execute();
-            } else if (numResults === length) {
-              isFinished = true;
-              resolve(results);
-            }
-          }
-        })
-        .catch((err) => {
-          if (!isFinished) {
-            isFinished = true;
-            reject(err);
-          }
-        });
-    };
-    while (numExecutions < maxExecutions) void execute();
-  });
+  const executions = [...arr];
+  const results: Value[] = [];
+  if (!maxExecutions) return Promise.resolve(results);
+  await Promise.all(
+    Array.from({ length: maxExecutions }).map(async () => {
+      while (executions.length) {
+        const index = length - executions.length;
+        const execution = executions.shift();
+        if (execution) {
+          results[index] = await execution();
+        }
+      }
+    }),
+  );
+  return results;
 }
 
 /**
  * Clamped alternative to Promise.allSettled.
  */
-export function clampedAllSettled<Value, Reason = Error>(
+export async function clampedAllSettled<Value, Reason = Error>(
   arr: Array<() => Promise<Value> | Value>,
   clamp: number,
 ): Promise<SettledReturnType<Value, Reason>[]> {
   const { length } = arr;
   const maxExecutions = Math.min(length, clamp);
-  return maxExecutions === 0 ? Promise.resolve([]) : new Promise((resolve) => {
-    const results: SettledReturnType<Value, Reason>[] = Array.from({ length });
-    let numExecutions = 0;
-    let numResults = 0;
-    const execute = () => {
-      const index = numExecutions++;
-      return Promise.resolve()
-        .then(() => arr[index]())
-        .then((result) => {
-          results[index] = { status: 'fulfilled', value: result };
-        })
-        .catch((err) => {
-          results[index] = { status: 'rejected', reason: err };
-        })
-        .then(() => {
-          numResults++;
-          if (numExecutions < length) void execute();
-          else if (numResults === length) resolve(results);
-        });
-    };
-    while (numExecutions < maxExecutions) void execute();
-  });
+  const executions = [...arr];
+  const results: SettledReturnType<Value, Reason>[] = [];
+  if (!maxExecutions) return Promise.resolve(results);
+  await Promise.all(
+    Array.from({ length: maxExecutions }).map(async () => {
+      while (executions.length) {
+        const index = length - executions.length;
+        const execution = executions.shift();
+        if (execution) {
+          try {
+            results[index] = { status: 'fulfilled', value: await execution() };
+          } catch (err) {
+            results[index] = { status: 'rejected', reason: err };
+          }
+        }
+      }
+    }),
+  );
+  return results;
 }
